4e4b78140bdd1365c06e547b6e43ca6a411f25d7
Xiang Wang <ramwin@qq.com>
2020-12-07 17:18:07 +0800

    使用vuex获取异步的数据

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -2,8 +2,15 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <h1>测试vuex</h1>
-    <p>local: {{local}}</p>
-    <p>count: {{count}}</p>
+    <p>count: {{count}}<button @click="incr">加1</button></p>
+    <p>count: {{count}}<button @click="incr_asyc">异步加10</button></p>
+    <p>count: {{count}}<button @click="incr_asyc_back">异步加1后回调</button></p>
+    <ul>
+      <li v-for="todo in todos" :key="todo">{{todo}}</li>
+    </ul>
+    <p><button @click="add_todo()">点我添加任务</button></p>
+    <p><button @click="get_third_todo()">点我先创建后获取第三条</button></p>
+    <p><button @click="get_third_todo2()">点我直接获取第三条</button></p>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -20,8 +27,40 @@ export default {
     count () {
       return this.$store.state.count
     },
+    todos() {
+      return this.$store.state.todos
+    },
     local() {
       return 3
+    },
+  },
+  methods: {
+    incr: function() {
+      this.$store.commit('increment')
+    },
+    incr_asyc: function() {
+      this.$store.dispatch('increment')
+    },
+    incr_asyc_back: function() {
+      this.$store.dispatch('increment_callback').then(() => {
+        console.info("修改成功")
+        console.info(this.count)
+      })
+    },
+    add_todo: function() {
+      this.$store.dispatch('add_todo')
+    },
+    get_third_todo: function() {
+      this.$store.dispatch('add_todo').then(() =>{
+        console.info("第三条任务是: ")
+        console.info(this.$store.getters.third_todo)
+      })
+    },
+    get_third_todo2: function() {
+      this.$store.dispatch('get_third_todo').then(() =>{
+        console.info("第三条任务是: ")
+        console.info(this.$store.getters.third_todo)
+      })
     },
   },
 }
--- hello-world/src/main.js
+++ hello-world/src/main.js
@@ -8,12 +8,66 @@ Vue.config.productionTip = false
 
 const store = new Vuex.Store({
   state: {
-    count: 9527
+    count: 9527,
+    todos: [
+      "待办事项1",
+      "待办事项2",
+    ],
+  },
+  getters: {
+    third_todo: state => {
+      if (state.todos.length < 3) {
+        this.commit('add_todo')
+      }
+      return state.todos[2]
+    }
   },
   mutations: {
     increment (state) {
       state.count++
-    }
+    },
+    add_todo(state) {
+      state.todos.push('新的代办事项')
+    },
+  },
+  actions: {
+    increment(context) {
+      for (let i=0; i<10; i++) {
+        setTimeout(
+          ()=>{
+            context.commit('increment')
+          },
+          1000*i
+        )
+      }
+    },
+    increment_callback(context) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          context.commit('increment')
+          resolve()
+        }, 1000)
+      })
+    },
+    add_todo(context) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          context.commit('add_todo')
+          resolve()
+        }, 1000)
+      })
+    },
+    get_third_todo(context) {
+      return new Promise((resolve, reject) => {
+        if (context.state.todos.length >= 3) {
+          resolve()
+        } else {
+          return context.dispatch("add_todo").then(() => {
+            resolve()
+          })
+        }
+      })
+    },
   }
 })
 
